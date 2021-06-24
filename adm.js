184812e6577720dec347bf7e7e0a8e2c02501dbf
Rodrigo Lobenwein <62033829+rlobenwein@users.noreply.github.com>
2021-06-24 15:46:25 -0300

    Commit inicial

---
+++ adm.js
@@ -0,0 +1,40 @@
+
+document.getElementById("tabela-usuarios").onload=listUser();
+
+function listUser() {
+    var tabUser = document.getElementById("tabela-usuarios");
+    var db_usuarios = JSON.parse(localStorage.getItem("db_usuarios"));
+    for (var i = 0; i < db_usuarios.usuarios.length; i++) {
+        var usuario = db_usuarios.usuarios[i];
+                
+        console.log("i: "+i);
+        tabUser.innerHTML += `
+            <tr>
+                <th scope="row">${i + 1}</th>
+                <td>${usuario.nome}</td>
+                <td>${usuario.login}</td>
+                <td>${usuario.email}</td>
+            </tr>`       
+    }      
+}
+
+function uploadFile() {
+    alert("teste");
+    const formData = new FormData();
+    const fileField = document.querySelector('input[type="file"]');
+
+    formData.append('username', 'abc123');
+    formData.append('avatar', fileField.files[0]);
+
+    fetch('imgs/galeria', {
+        method: 'Post',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Success:', result);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+}
